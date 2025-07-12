d8269f57f0f77bf50a73ee472cd572f4a3d3126f
Abraham Muthiani <muthianiabraham254@gmail.com>
2025-07-12 14:30:42 +0300

    splashscreen

--- Lift_Builder/src/router/index.js
+++ Lift_Builder/src/router/index.js
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SplashScreen from '../views/SplashScreen.vue'
+import LiftBuilder from '../views/LiftBuilder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'SplashScreen',
+      component: SplashScreen,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/LiftBuilder',
+      name: 'LiftBuilder',
+      component: LiftBuilder,
     },
   ],
 })
---
+++ Lift_Builder/src/views/LiftBuilder.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Lift Builder</h1>
+        <p>Welcome to the Lift Builder application!</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
